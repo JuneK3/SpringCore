629ca673bb688e988968c08c0664313d05ed5f70
Junek3 <rlarudrms28@gmail.com>
2022-09-21 13:00:05 +0900

    first commit

---
+++ src/test/java/com/rootlab/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootlab.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
