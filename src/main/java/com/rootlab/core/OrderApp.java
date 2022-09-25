6958b39c3b29b46fbcade4cdda0277f11329972c
Junek3 <rlarudrms28@gmail.com>
2022-09-25 18:28:32 +0900

    스프링으로 전환하기

--- src/main/java/com/rootlab/core/OrderApp.java
+++ src/main/java/com/rootlab/core/OrderApp.java
@@ -7,13 +7,19 @@ import com.rootlab.core.member.MemberServiceImpl;
 import com.rootlab.core.order.Order;
 import com.rootlab.core.order.OrderService;
 import com.rootlab.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
-		OrderService orderService = appConfig.orderService();
-		MemberService memberService = appConfig.memberService();
+//		AppConfig appConfig = new AppConfig();
+//		OrderService orderService = appConfig.orderService();
+//		MemberService memberService = appConfig.memberService();
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+		OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
 		long memberId = 1L;
 		Member member = new Member(memberId, "memberA", Grade.VIP);
