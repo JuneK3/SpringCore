c272741947a556be5d2a3b2a7f982b079555491f
Junek3 <rlarudrms28@gmail.com>
2022-09-29 23:26:35 +0900

    @Bean에 빈 생명주기 콜백 등록하기

--- src/test/java/com/rootlab/core/lifecycle/NetworkClient.java
+++ src/test/java/com/rootlab/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package com.rootlab.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 	private String url;
 
 	public NetworkClient() {
@@ -27,16 +24,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 		System.out.println("close: " + url);
 	}
 
-	@Override // 빈 생성 -> 의존관계 주입 후 실행
-	public void afterPropertiesSet() throws Exception {
-		System.out.println("NetworkClient.afterPropertiesSet");
+	public void init() {
+		System.out.println("NetworkClient.init");
 		connect();
 		call("초기화 연결 메세지");
 	}
 
-	@Override
-	public void destroy() throws Exception {
-		System.out.println("NetworkClient.destroy");
+	public void close() {
+		System.out.println("NetworkClient.close");
 		disconnect();
 	}
 
